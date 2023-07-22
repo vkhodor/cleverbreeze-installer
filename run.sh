1d1f9671e43989563394c09ace1027bd8708a8cb
Viktor V. Khodorchenko <v.hodorchenko@emergingtravel.com>
2023-07-22 22:35:09 +0700

    fix

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "[INFO] Installing Ansible and deps"
 apt-get update
 apt-get install ansible -y
 echo "[INFO] Turning SNMPAgg off"
-ansible-playbook ./10_turn_off_snmpagg.yml
+ansible-playbook ./10_turn_snmpagg_off.yml
 ansible-playbook ./install.yml
 
 
