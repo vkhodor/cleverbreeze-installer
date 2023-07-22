ddc32895f9e1f602b73c9b283cfea8f1e12dae3c
Viktor V. Khodorchenko <v.hodorchenko@emergingtravel.com>
2023-07-22 22:52:13 +0700

    add ssh config

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ apt-get update
 apt-get install ansible -y
 echo "[INFO] Turning SNMPAgg off"
 ansible-playbook ./10_turn_snmpagg_off.yml
-ansible-playbook ./install.yml
+ansible-playbook ./install.yml --ask-vault-pass
 
 
